e0c18499ffa8533e55e9cd74d5e4240b70716bf9
David <david.windler@windowslive.com>
2018-02-25 22:45:00 +0100

    Products section

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -6,8 +6,8 @@ const hash = require('gulp-hash');
 gulp.task('css', () => {
     return gulp
       .src('./app/assets/styles/main.sass')
-      .pipe(sass({outputStyle: 'compressed', includePaths: [ './node_modules/normalize-scss/sass' ]}).on('error', sass.logError))
-      // .pipe(sass({includePaths: [ './node_modules/normalize-scss/sass' ]}).on('error', sass.logError))
+      // .pipe(sass({outputStyle: 'compressed', includePaths: [ './node_modules/normalize-scss/sass' ]}).on('error', sass.logError))
+      .pipe(sass({includePaths: [ './node_modules/normalize-scss/sass' ]}).on('error', sass.logError))
       .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
